5a500b94ce8cf59db988990085675ad6e0b7411d
hugo <hugo.perier@cern.ch>
2022-09-23 23:45:29 +0200

    wip connection menu modal

--- src/components/Connection/RobotConnection.tsx
+++ src/components/Connection/RobotConnection.tsx
@@ -5,6 +5,7 @@ import RobotModuleIcon from "../RobotModuleIcon"
 import FmdGoodIcon from '@mui/icons-material/FmdGood';
 import Battery80Icon from '@mui/icons-material/Battery80';
 import { useState } from "react";
+import RobotConnectionModal from "./RobotConnectionModal";
 
 const useStyles = makeStyles(() => ({
     root: {
@@ -46,45 +47,52 @@ const RobotConnection = (props: IRobotConnectionProps) => {
         setConnectionModalOpen(true)
     }
 
+    const handleCloseConnectionModal = () => {
+        setConnectionModalOpen(false)
+    }
+
     return (
-        <div className={classes.root}>
-            <h2>{robotConnection.name}</h2>
-            <p>{robotConnection.status}</p>
-            <div className={classes.cardBody}>
-                <Badge badgeContent=" " color="primary"
-                    anchorOrigin={
-                        {
-                            vertical: 'bottom',
-                            horizontal: 'right',
+        <>
+        {connectionModalOpen && <RobotConnectionModal open={connectionModalOpen} onClose={handleCloseConnectionModal} connection={robotConnection} />}
+            <div className={classes.root}>
+                <h2>{robotConnection.name}</h2>
+                <p>{robotConnection.status}</p>
+                <div className={classes.cardBody}>
+                    <Badge badgeContent=" " color="primary"
+                        anchorOrigin={
+                            {
+                                vertical: 'bottom',
+                                horizontal: 'right',
+                            }
                         }
-                    }
-                >
-                    <img src={require(`../../../assets/${robotConnection.type}.png`)} width={150} alt="robot-icon" />
-                </Badge>
-                <div className={classes.partIcons}>
-                    {robotConnection.parts.map((part) => {
-                        return (
-                            <RobotModuleIcon type={part.type} width={24} height={24} title={part.name} key={part.id} />
-                        )
-                    })}
-                </div>
-                <div>
-                    <div>
-                        <FmdGoodIcon />
-                        <span>Home</span>
+                    >
+                        <img src={require(`../../../assets/${robotConnection.type}.png`)} width={150} alt="robot-icon" />
+                    </Badge>
+                    <div className={classes.partIcons}>
+                        {robotConnection.parts.map((part) => {
+                            return (
+                                <RobotModuleIcon type={part.type} width={24} height={24} title={part.name} key={part.id} />
+                            )
+                        })}
                     </div>
                     <div>
-                        <Battery80Icon />
-                        <span>100%</span>
+                        <div>
+                            <FmdGoodIcon />
+                            <span>Home</span>
+                        </div>
+                        <div>
+                            <Battery80Icon />
+                            <span>100%</span>
+                        </div>
                     </div>
-                </div>
-                <div className={classes.connectButton}>
-                    <Button variant="contained" onClick={handleConnectClick}>
-                        Connect
-                    </Button>
+                    <div className={classes.connectButton}>
+                        <Button variant="contained" onClick={handleConnectClick}>
+                            Connect
+                        </Button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
--- src/components/Connection/RobotConnectionModal.tsx
+++ src/components/Connection/RobotConnectionModal.tsx
@@ -1,8 +1,71 @@
-import { Badge, Modal, Switch } from "@mui/material"
+import { Badge, Button, MenuItem, Modal, Paper, Select, Switch } from "@mui/material"
 import { IRobotConnection } from "../../network/IRobot"
 import FmdGoodIcon from '@mui/icons-material/FmdGood';
 import Battery80Icon from '@mui/icons-material/Battery80';
 import RobotModuleIcon from "../RobotModuleIcon";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles(() => ({
+    root: {
+        width: '500px',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        "& h2,h3": {
+            textAlign: 'center',
+        },
+    },
+    groupStatus: {
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
+    paper: {
+        position: 'relative',
+    },
+    image: {
+        width: '150px',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        display: 'block',
+    },
+    groupNetwork: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    networkField: {
+        "& *": {
+            display: 'inline-block',
+            marginBottom: '0',
+        },
+        "& p": {
+            marginLeft: '30px',
+        }
+    },
+    robotParts: {
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        "& h4": {
+            width: '100px',
+        }
+    },
+    layoutSelect: {
+        width: '300px',
+        marginBottom: '20px',
+        marginTop: '20px',
+    },
+    selectContainer: {
+        textAlign: 'center',
+    },
+    button: {
+        position: 'absolute',
+        bottom: '0',
+        width: '100%',
+    }
+}))
 
 export interface RobotConnectionModalProps {
     open: boolean
@@ -12,52 +75,70 @@ export interface RobotConnectionModalProps {
 
 const RobotConnectionModal = (props: RobotConnectionModalProps) => {
     const { open, onClose, connection } = props
+    const classes = useStyles()
 
     return (
         <Modal
             open={open}
             onClose={onClose}
         >
-            <div>
-                <h2>{connection.name}</h2>
-                <div>
-                    <Badge>
-                        <p>{connection.status}</p>
-                    </Badge>
-                    <div>
-                        <Battery80Icon />
-                        <span>100%</span>
-                    </div>
-                    <div>
-                        <FmdGoodIcon />
-                        <span>Home</span>
-                    </div>
-                </div>
-                <img src={require(`../../../assets/${connection.type}.png`)} width={150} alt="robot-icon" />
-                <div>
-                    <div>
-                        <h4>Connection</h4>
-                        <p>{connection.connection.type}</p>
-                    </div>
-                    <div>
-                        <h4>Host</h4>
-                        <p>{connection.connection.hostname}</p>
-                    </div>
-                    <div>
-                        <h4>Port</h4>
-                        <p>{connection.connection.port}</p>
-                    </div>
-                </div>
-                <h3>Robot components</h3>
-                <div>
-                    {connection.parts.map((part) => (
+            <div className={classes.root}>
+                <Paper elevation={3} className={classes.paper}>
+                    <h2>{connection.name}</h2>
+                    <div className={classes.groupStatus}>
+                        <Badge badgeContent=" " color="primary" anchorOrigin={{ horizontal: "right", vertical: "bottom" }}>
+                            <p>{connection.status}</p>
+                        </Badge>
                         <div>
-                            <RobotModuleIcon type={part.type} title={part.name} width={24} height={24} />
-                            <h4>{part.name}</h4>
-                            <Switch />
+                            <Battery80Icon />
+                            <span>100%</span>
                         </div>
-                    ))}
-                </div>
+                        <div>
+                            <FmdGoodIcon />
+                            <span>Home</span>
+                        </div>
+                    </div>
+                    <img className={classes.image} src={require(`../../../assets/${connection.type}.png`)} width={150} alt="robot-icon" />
+                    <div className={classes.groupNetwork}>
+                        <div>
+                            <div className={classes.networkField}>
+                                <h4>Connection</h4>
+                                <p>{connection.connection.type}</p>
+                            </div>
+                            <div className={classes.networkField}>
+                                <h4>Host</h4>
+                                <p>{connection.connection.hostname}</p>
+                            </div>
+                            <div className={classes.networkField}>
+                                <h4>Port</h4>
+                                <p>{connection.connection.port}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <h3>Robot components</h3>
+                    <div>
+                        {connection.parts.map((part) => (
+                            <div key={part.id} className={classes.robotParts}>
+                                <RobotModuleIcon type={part.type} title={part.name} width={24} height={24} />
+                                <h4>{part.name}</h4>
+                                <Switch defaultChecked />
+                            </div>
+                        ))}
+                    </div>
+                    <div className={classes.selectContainer}>
+                        <Select defaultValue={1} className={classes.layoutSelect}>
+                            <MenuItem value={1}>Standard Layout</MenuItem>
+                            <MenuItem value={2}>Customized Layout #1</MenuItem>
+                        </Select>
+                    </div>
+                    <Button
+                        className={classes.button}
+                        variant="contained"
+                        color="primary"
+                    >
+                        Connect
+                    </Button>
+                </Paper>
             </div>
         </Modal>
     )
