cb9f592d26ee00d7a8bc552a79d31eac24272359
hugo <hugo.perier@cern.ch>
2022-12-31 15:22:54 +0100

    fix test and add describe section

--- __tests__/ViewManager.test.js
+++ __tests__/ViewManager.test.js
@@ -2,40 +2,23 @@ import React from "react";
 import { render, fireEvent, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import ViewManager from "../src/views/ViewManager";
+import { TabProvider } from "../src/contexts/TabContext";
 
-test("ViewManager render successfuly", async () => {
-  render(<ViewManager />);
-  const t = await screen.findByText(/Connect to a robot/);
-  expect(t).toBeVisible();
+describe("View Manager", () => {
+  test("ViewManager render successfuly", async () => {
+    render(<ViewManager />);
+    const t = await screen.findByText(/Connect to a robot/);
+    expect(t).toBeVisible();
+  });
+
+  test("ViewManager click home", async () => {
+    render(
+      <TabProvider>
+        <ViewManager />
+      </TabProvider>
+    );
+    const button = await screen.findByLabelText("home-menu");
+    fireEvent.click(button);
+    expect(button).toBeVisible();
+  });
 });
-
-test("ViewManager click home", async () => {
-  render(<ViewManager />);
-  const button = await screen.findByLabelText("home-menu");
-  fireEvent.click(button);
-  expect(button).toBeVisible();
-});
-
- test.todo("One robot connection displayed") //, () => {
-//   render(<ViewManager />);
-
-//   const coreInfos = {
-//     hostname: "192.168.1.1",
-//     port: "9090",
-//     type: RobotConnectionType.ROSBRIDGE,
-//   };
-//   const core = new Core(coreInfos);
-//   core.contextConfiguration = coreInfos;
-//   core.modules = [
-//     {
-//       id: "base",
-//       name: "base",
-//       type: "robotbase",
-//     },
-//   ];
-
-//   act(() => {
-//     // eslint-disable-next-line no-undef
-//     setCoreConnections([core]);
-//   });
-// });
