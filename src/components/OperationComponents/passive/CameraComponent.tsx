40d5c46cdf501a2a6f8ebf00a93941cf4a98b4e0
Hugo Perier <hugo.perier@protonmail.com>
2023-05-19 10:50:07 +0200

    12 camera component generic design (#15)
    * WebRTC Camera working
    
    * fix camera tests
    
    ---------
    
    Co-authored-by: hugo <hugo.perier@cern.ch>

--- src/components/OperationComponents/passive/CameraComponent.tsx
+++ src/components/OperationComponents/passive/CameraComponent.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@mui/material"
+import { Button, MenuItem, Select, SelectChangeEvent, Slider } from "@mui/material"
 import { makeStyles } from "@mui/styles"
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
 import { IOperationComponentDescriptor, IOperationComponentSpecifics } from "../IOperationComponents"
-import { Camera } from "neutron-core"
+import { Camera, CameraInfoUpdate, Resolution } from "neutron-core"
 import { useConnection } from "../../../contexts/MultiConnectionProvider";
 import React from "react";
 
@@ -11,6 +11,37 @@ const useStyles = makeStyles(() => ({
     root: {
         height: "100%",
         width: "100%",
+        position: 'relative'
+    },
+    cameraOverlay: {
+        position: 'absolute',
+        top: 0,
+        height: "100%",
+        width: "100%",
+        opacity: 0,
+        transition: "opacity 0.3s ease-in-out",
+        '&:hover': {
+            opacity: 1,
+            background: 'rgba(0,0,0,0.3)'
+        }
+    },
+    selectResolution: {
+        width: '90%'
+    },
+    rightOverlayPanel: {
+        width: '20%',
+        marginLeft: 'auto',
+        '& h4': {
+            color: 'white',
+            marginBottom: 0
+        }
+    },
+    leftOverlayPanel: {
+        position: 'absolute',
+        '& h4': {
+            color: 'white',
+            marginBottom: 0
+        }
     },
     streamer: {
         height: "100%",
@@ -28,6 +59,21 @@ const CameraComponent = (props: IOperationComponentSpecifics<ICameraComponentSpe
     const [isConnected, setIsConnected] = useState(specifics?.isConnected ?? false)
     const connection = useConnection(connectionId ?? "")
     const camera = connection?.modules.find(m => m.id === moduleId) as Camera | undefined
+    const [fps, setFps] = useState(15)
+    const [resolution, setResolution] = useState<Resolution | undefined>()
+
+    useEffect(() => {
+        if (!camera)
+            return
+        const handleCameraInfoUpdated = (infos: CameraInfoUpdate) => {
+            setFps(infos.fps)
+            setResolution(infos.resolution)
+        }
+        camera.infoUpdated.on(handleCameraInfoUpdated)
+        return () => {
+            camera.infoUpdated.off(handleCameraInfoUpdated)
+        }
+    }, [camera])
 
     useEffect(() => {
         if (!onCommitComponentSpecific) return
@@ -49,6 +95,26 @@ const CameraComponent = (props: IOperationComponentSpecifics<ICameraComponentSpe
         setIsConnected(false)
     }
 
+    const handleFPSChange = async (_: any, value: number | number[]) => {
+        if (typeof value !== 'number')
+            return
+        console.log("set FPS ", value)
+        const res = await camera?.setFps(value)
+        console.log("alors ? ", res)
+    }
+
+    const handleResolutionChange = async (event: SelectChangeEvent) => {
+        if (!camera)
+            return
+        const newResolution = camera.resolutions.find(e => `${e?.width}x${e?.height}` === event.target.value)
+        if (!newResolution)
+            return
+        const res = await camera.setResolution(newResolution)
+        console.log("res is ", res)
+        if (res)
+            setResolution(newResolution)
+    }
+
     return (
         <div className={classes.root}>
             {camera && isConnected ? (
@@ -57,6 +123,35 @@ const CameraComponent = (props: IOperationComponentSpecifics<ICameraComponentSpe
                     <Button variant="contained" onClick={handleOnDisconnect} aria-label="disconnect-cmd">
                         Disconnect
                     </Button>
+                    <div className={classes.cameraOverlay}>
+                        <div className={classes.leftOverlayPanel}>
+                            <h4>{`FPS: ${fps}`}</h4>
+                        </div>
+                        <div className={classes.rightOverlayPanel}>
+                            <h4>{`FPS`}</h4>
+                            <Slider
+                                onChangeCommitted={handleFPSChange}
+                                aria-labelledby="input-slider"
+                                defaultValue={15}
+                                min={5}
+                                max={30}
+                                valueLabelDisplay="auto"
+                            />
+                            <h4>{`Resolution`}</h4>
+                            <Select
+                                labelId="resolution-select"
+                                value={`${resolution?.width}x${resolution?.height}`}
+                                label="Resolution"
+                                className={classes.selectResolution}
+                                onChange={handleResolutionChange}
+                            >
+                                {camera.resolutions.map((res) => (
+                                    <MenuItem value={`${res.width}x${res.height}`}>{res.width}p</MenuItem>
+                                ))}
+                            </Select>
+                        </div>
+
+                    </div>
                 </>
             ) : (
                 <Button variant="contained" onClick={handleOnConnect} aria-label="connect-cmd">
